94cb9b362d05cbd49cac562d732f3c278a1cbe69
jeanagnissan <yoanefirst@gmail.com>
2023-03-03 14:25:24 +0000

    initialization of the project

---
+++ src/test/java/africa/soimpaf/api_v1_303/ApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.soimpaf.api_v1_303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
